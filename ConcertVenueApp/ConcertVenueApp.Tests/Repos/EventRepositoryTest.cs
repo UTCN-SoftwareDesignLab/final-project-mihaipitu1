3a2898c195685cd695c72f4ef532d97f13f38583
Mihai Pitu <mihai.pitu@accenture.com>
2018-05-29 16:29:24 +0300

    Added some unit tests there. And it's done

---
+++ ConcertVenueApp/ConcertVenueApp.Tests/Repos/EventRepositoryTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConcertVenueApp.Database;
+using ConcertVenueApp.Models;
+using ConcertVenueApp.Models.Builders;
+using ConcertVenueApp.Repositories.Events;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ConcertVenueApp.Tests.Repos
+{
+    [TestClass]
+    public class EventRepositoryTest
+    {
+        private IEventRepository eventRepo;
+
+        public EventRepositoryTest()
+        {
+            eventRepo = new EventRepositoryMySQL(new DBConnectionFactory().GetConnectionWrapper(true));
+        }
+
+        public void RemoveAll()
+        {
+            List<Event> users = eventRepo.FindAll();
+
+            foreach (var user in users)
+            {
+                eventRepo.Delete(user);
+            }
+        }
+
+        [TestMethod]
+        public void TestFindAllUsers()
+        {
+            RemoveAll();
+            List<Event> users = eventRepo.FindAll();
+            Assert.AreEqual(users.Count, 0);
+        }
+
+        [TestMethod]
+        public void TestCreateUser()
+        {
+            RemoveAll();
+            Event user = new EventBuilder()
+                .SetId(1)
+                .SetTitle("title")
+                .SetGenre("genre")
+                .SetDate(DateTime.Now)
+                .SetDescription("Des")
+                .SetNoTickets(1)
+                .SetPrice(1.0)
+                .Build();
+            Assert.IsTrue(eventRepo.Create(user));
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestEditUser()
+        {
+            RemoveAll();
+            Event user = new EventBuilder()
+                .SetId(1)
+                .SetTitle("title")
+                .SetGenre("genre")
+                .SetDate(DateTime.Now)
+                .SetDescription("Des")
+                .SetNoTickets(1)
+                .SetPrice(1.0)
+                .Build();
+            eventRepo.Create(user);
+            Assert.IsTrue(eventRepo.Update(user));
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestDeleteUser()
+        {
+            RemoveAll();
+            Event user = new EventBuilder()
+                .SetId(1)
+                .SetTitle("title")
+                .SetGenre("genre")
+                .SetDate(DateTime.Now)
+                .SetDescription("Des")
+                .SetNoTickets(1)
+                .SetPrice(1.0)
+                .Build();
+            eventRepo.Create(user);
+            Assert.IsTrue(eventRepo.Delete(user));
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestFindEventById()
+        {
+            Event user = eventRepo.FindById(0);
+            Assert.IsNull(user.GetTitle());
+        }
+    }
+}
---
+++ ConcertVenueApp/ConcertVenueApp.Tests/Repos/UserRepositoryTest.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConcertVenueApp.Database;
+using ConcertVenueApp.Models;
+using ConcertVenueApp.Models.Builders;
+using ConcertVenueApp.Repositories.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConcertVenueApp.Tests.Repos
+{
+
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        private IUserRepository userRepo;
+
+        public UserRepositoryTest()
+        {
+            userRepo = new UserRepositoryMySQL(new DBConnectionFactory().GetConnectionWrapper(true));
+        }
+
+        public void RemoveAll()
+        {
+            List<User> users = userRepo.FindAll();
+
+            foreach (var user in users)
+            {
+                userRepo.Delete(user);
+            }
+        }
+
+        [TestMethod]
+        public void TestFindAllUsers()
+        {
+            RemoveAll();
+            List<User> users = userRepo.FindAll();
+            Assert.AreEqual(users.Count, 0);
+        }
+
+        [TestMethod]
+        public void TestCreateUser()
+        {
+            RemoveAll();
+            User user = new UserBuilder()
+                .SetId(1)
+                .SetName("name")
+                .SetUsername("User")
+                .SetPassword("Password")
+                .SetType("admin")
+                .Build();
+            Assert.IsTrue(userRepo.Create(user));
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestEditUser()
+        {
+            RemoveAll();
+            User user = new UserBuilder()
+                .SetId(1)
+                .SetName("name")
+                .SetUsername("User")
+                .SetPassword("Password")
+                .SetType("admin")
+                .Build();
+            userRepo.Create(user);
+            Assert.IsTrue(userRepo.Update(user));
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestDeleteUser()
+        {
+            RemoveAll();
+            User user = new UserBuilder()
+                .SetId(1)
+                .SetName("name")
+                .SetUsername("User")
+                .SetPassword("Password")
+                .SetType("admin")
+                .Build();
+            userRepo.Create(user);
+            Assert.IsTrue(userRepo.Delete(user));
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestFindUserById()
+        {
+            User user = userRepo.FindById(0);
+            Assert.IsNull(user.GetName());
+        }
+
+        [TestMethod]
+        public void TestFindUserByUsernameAndPassword()
+        {
+            User user = userRepo.FindByUsernameAndPassword("user", "pass");
+            Assert.IsNull(user.GetName());
+        }
+
+        [TestMethod]
+        public void TestFindAllUsersNotNull()
+        {
+            RemoveAll();
+            User user = new UserBuilder()
+                .SetId(1)
+                .SetName("name")
+                .SetUsername("User")
+                .SetPassword("Password")
+                .SetType("admin")
+                .Build();
+            userRepo.Create(user);
+            user.SetId(user.GetId() + 1);
+            userRepo.Create(user);
+            user.SetId(user.GetId() + 1);
+            userRepo.Create(user);
+            List<User> users = userRepo.FindAll();
+            Assert.AreEqual(users.Count, 3);
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestFindUserByIdNotNull()
+        {
+            RemoveAll();
+            User user = new UserBuilder()
+                .SetId(1)
+                .SetName("name")
+                .SetUsername("User")
+                .SetPassword("Password")
+                .SetType("admin")
+                .Build();
+            userRepo.Create(user);
+            User users = userRepo.FindById(1);
+            Assert.IsNotNull(users);
+            RemoveAll();
+        }
+
+        [TestMethod]
+        public void TestFindUserByUsernameAndPasswordNotNull()
+        {
+            RemoveAll();
+            User user = new UserBuilder()
+                .SetId(1)
+                .SetName("name")
+                .SetUsername("User")
+                .SetPassword("Password")
+                .SetType("admin")
+                .Build();
+            userRepo.Create(user);
+            User users = userRepo.FindByUsernameAndPassword("User", "Password");
+            Assert.IsNotNull(users);
+            RemoveAll();
+        }
+    }
+}
